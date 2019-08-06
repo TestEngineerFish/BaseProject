16f949917a6ea0de03898aaac39f9794c1e4d2bc
shatingyu <916878440@qq.com>
2019-08-06 19:01:16 +0800

    添加Bugly 增加字体

---
+++ BaseProject/Utils/AlertView/BPBaseAlertView.swift
@@ -0,0 +1,119 @@
+//
+//  BPBaseAlertView.swift
+//  BaseProject
+//
+//  Created by 沙庭宇 on 2019/8/6.
+//  Copyright © 2019 沙庭宇. All rights reserved.
+//
+
+import UIKit
+
+/// AlertView的基类,默认只显示标题或者标题+描述信息
+class BPBaseAlertView: BPTopWindowView {
+
+    // 弹框中描述文字高度
+    var descriptionHeight: CGFloat = 0.0
+
+    // 弹框的背景
+    var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor     = UIColor.white
+        view.layer.cornerRadius  = 20
+        view.layer.masksToBounds = true
+        return view
+    }()
+
+    // 弹窗标题
+    var titleLabel: UILabel = {
+        let label           = UILabel()
+        label.numberOfLines = 1
+        label.textColor     = UIColor.black1
+        label.font          = UIFont.regularFont(ofSize: 16)
+        label.textAlignment = .center
+        return label
+    }()
+
+    // 弹窗描述
+    var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.black1.withAlphaComponent(0.5)
+        label.font = UIFont.regularFont(ofSize: 16)
+        label.numberOfLines = 0
+        label.textAlignment = .left
+        return label
+    }()
+
+    // 左边按钮
+    var leftButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage.imageWithColor(UIColor.gray3), for: .normal)
+        button.setTitleColor(UIColor.black1, for: .normal)
+        button.titleLabel?.font    = UIFont.mediumFont(ofSize: 18)
+        button.layer.cornerRadius  = 10
+        button.layer.masksToBounds = true
+        button.addTarget(self, action: #selector(leftBtnAction), for: .touchUpInside)
+        return button
+    }()
+
+    // 右边按钮
+    var rightButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage.imageWithColor(UIColor.green1), for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font    = UIFont.mediumFont(ofSize: 18)
+        button.layer.cornerRadius  = 10
+        button.layer.masksToBounds = true
+        button.addTarget(self, action: #selector(rightBtnAction), for: .touchUpInside)
+        return button
+    }()
+
+    // 关闭按钮
+    var closeButton: UIButton = {
+        let button = UIButton()
+        button.setTitle(Iconfont.close1.rawValue, for: .normal)
+        button.setTitleColor(UIColor.black1.withAlphaComponent(0.8), for: .normal)
+        button.titleLabel?.font = UIFont.iconFont(size: 40)
+        button.addTarget(self, action: #selector(closeBtnAction), for: .touchUpInside)
+        button.isHidden = true
+        return button
+    }()
+
+    // 图片
+    var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = UIView.ContentMode.scaleAspectFill
+        return imageView
+    }()
+
+    override func setupSubviews() {
+        super.setupSubviews()
+        self.addSubview(backgroundView)
+        self.addSubview(containerView)
+        self.addSubview(closeButton)
+
+        containerView.addSubview(titleLabel)
+        containerView.addSubview(descriptionLabel)
+        containerView.addSubview(leftButton)
+        containerView.addSubview(rightButton)
+        containerView.addSubview(imageView)
+
+        containerView.snp.makeConstraints { (make) in
+            make.center.equalTo(self)
+            make.width.equalTo(kScreenWidth - 60)
+        }
+
+        closeButton.snp.makeConstraints { (make) in
+            make.size.equalTo(CGSize(width: 60, height: 60))
+            make.top.equalTo(containerView.snp.bottom).offset(15)
+            make.centerX.equalToSuperview()
+        }
+    }
+
+    @objc func leftBtnAction() {
+        self.removeFromSuperview()
+    }
+
+    @objc func rightBtnAction() {
+        self.removeFromSuperview()
+    }
+}
--- BaseProject/Utils/BPTopWindowView.swift
+++ BaseProject/Utils/BPTopWindowView.swift
@@ -8,10 +8,8 @@
 
 import SnapKit
 
+/// 所有需要现在在顶部Window的视图,都需要继承该类
 class BPTopWindowView: UIView {
-
-    // 弹框中描述文字高度
-    var descriptionHeight: CGFloat = 0.0
 
     // 全屏透明背景
     lazy var backgroundView: UIView = {
@@ -23,70 +21,6 @@ class BPTopWindowView: UIView {
         return view
     }()
 
-    // 弹框的背景
-    var containerView: UIView = {
-        let view = UIView()
-        view.backgroundColor     = UIColor.white
-        view.layer.cornerRadius  = 20
-        view.layer.masksToBounds = true
-        return view
-    }()
-
-    // 弹窗标题
-    var titleLabel: UILabel = {
-        let label           = UILabel()
-        label.numberOfLines = 1
-        label.textColor     = UIColor.black1
-        label.font          = UIFont.regularFont(ofSize: 16)
-        label.textAlignment = .center
-        return label
-    }()
-
-    // 弹窗描述
-    var descriptionLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = UIColor.black1.withAlphaComponent(0.5)
-        label.font = UIFont.regularFont(ofSize: 16)
-        label.numberOfLines = 0
-        label.textAlignment = .left
-        return label
-    }()
-
-    // 左边按钮
-    var leftButton: UIButton = {
-        let button = UIButton()
-        button.setBackgroundImage(UIImage.imageWithColor(UIColor.gray3), for: .normal)
-        button.setTitleColor(UIColor.black1, for: .normal)
-        button.titleLabel?.font    = UIFont.mediumFont(ofSize: 18)
-        button.layer.cornerRadius  = 10
-        button.layer.masksToBounds = true
-        button.addTarget(self, action: #selector(leftBtnAction), for: .touchUpInside)
-        return button
-    }()
-
-    // 右边按钮
-    var rightButton: UIButton = {
-        let button = UIButton()
-        button.setBackgroundImage(UIImage.imageWithColor(UIColor.green1), for: .normal)
-        button.setTitleColor(UIColor.white, for: .normal)
-        button.titleLabel?.font    = UIFont.mediumFont(ofSize: 18)
-        button.layer.cornerRadius  = 10
-        button.layer.masksToBounds = true
-        button.addTarget(self, action: #selector(rightBtnAction), for: .touchUpInside)
-        return button
-    }()
-
-    // 右上角的关闭按钮
-    var closeButton: UIButton = {
-        let button = UIButton()
-        button.setTitle(Iconfont.close.rawValue, for: .normal)
-        button.setTitleColor(UIColor.black1.withAlphaComponent(0.5), for: .normal)
-        button.titleLabel?.font = UIFont.iconFont(size: 26)
-//        button.addTarget(self, action: #selector(closeBtnAction), for: .touchUpInside)
-        button.isHidden = true
-        return button
-    }()
-
     override init(frame: CGRect) {
         super.init(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: kScreenWidth, height: kScreenHeight)))
     }
@@ -96,81 +30,7 @@ class BPTopWindowView: UIView {
     }
 
     func setupSubviews() {
-        var viewHeight: CGFloat = 110
         self.addSubview(backgroundView)
-        self.addSubview(containerView)
-
-        containerView.snp.makeConstraints { (make) in
-            make.center.equalTo(self)
-            make.left.equalToSuperview().offset(30)
-            make.right.equalToSuperview().offset(-30)
-            make.height.equalTo(viewHeight + descriptionHeight)
-        }
-
-        containerView.addSubview(descriptionLabel)
-        containerView.addSubview(rightButton)
-        containerView.addSubview(closeButton)
-
-        // 是否显示标题
-        if let title = titleLabel.text, title.isNotEmpty {
-            containerView.addSubview(titleLabel)
-            viewHeight = 156.0
-            titleLabel.snp.makeConstraints { (make) in
-                make.top.equalToSuperview().offset(18)
-                make.left.equalToSuperview().offset(15)
-                make.right.equalToSuperview().offset(-15)
-                make.height.equalTo(22)
-            }
-
-            descriptionLabel.snp.makeConstraints { (make) in
-                make.top.equalTo(titleLabel.snp.bottom).offset(18)
-                make.left.right.equalTo(titleLabel)
-                make.height.equalTo(descriptionHeight)
-            }
-            
-            containerView.snp.updateConstraints { (make) in
-                make.height.equalTo(viewHeight + descriptionHeight)
-            }
-        } else {
-            descriptionLabel.snp.makeConstraints { (make) in
-                make.top.equalToSuperview().offset(18)
-                make.left.equalToSuperview().offset(15)
-                make.right.equalTo(-15)
-                make.height.equalTo(descriptionHeight)
-            }
-        }
-
-        // 是否显示左边按钮
-        if let leftBtnTitle = leftButton.titleLabel?.text, leftBtnTitle.isNotEmpty {
-            containerView.addSubview(leftButton)
-            leftButton.snp.makeConstraints { (make) in
-                make.bottom.equalToSuperview().offset(-18)
-                make.left.equalToSuperview().offset(15)
-                make.right.equalTo(containerView.snp.centerX).offset(-8)
-                make.height.equalTo(56)
-            }
-            rightButton.snp.makeConstraints { (make) in
-                make.bottom.equalToSuperview().offset(-18)
-                make.left.equalTo(containerView.snp.centerX).offset(8)
-                make.right.equalToSuperview().offset(-15)
-                make.height.equalTo(56)
-            }
-        } else {
-            rightButton.snp.makeConstraints { (make) in
-                make.bottom.equalToSuperview().offset(-18)
-                make.left.equalToSuperview().offset(15)
-                make.right.equalToSuperview().offset(-15)
-                make.height.equalTo(56)
-            }
-        }
-    }
-
-    @objc func leftBtnAction() {
-        self.removeFromSuperview()
-    }
-
-    @objc func rightBtnAction() {
-        self.removeFromSuperview()
     }
 
     @objc func closeBtnAction() {
