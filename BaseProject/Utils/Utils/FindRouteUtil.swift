9173516f4e4a43e671ee93d5c6a61e6a32b5f4d6
TestEngineerFish <916878440@qq.com>
2019-10-25 02:06:22 +0800

    完成了随机值路径推到算法

--- BaseProject/Utils/Utils/FindRouteUtil.swift
+++ BaseProject/Utils/Utils/FindRouteUtil.swift
@@ -11,64 +11,86 @@ import Foundation
 struct FindRouteUtil {
     // 矩阵是从0开始计算的
     let matrix = 4 // 4 x 4
-    var blackList = [Int]()
+    var blackList2 = Array(repeating: [Int](), count: 16)
     var routeList = [Int]()
     let word = "hellohellohelloe"
 
     static func getRoute(_ index: Int) -> [Int] {
         var util = FindRouteUtil()
-        util.findRoute(start: index)
-        print("------黑名单有:\(util.blackList)")
+        util.findRoute(index)
+        print("------黑名单有:\(util.blackList2)")
         return util.routeList
     }
     
     /// 返回数字周围的至
-    func nextStepList(_ num: Int) -> [Int] {
+    private func nextStepList(_ num: Int) -> [Int] {
         var neighbours = [Int]()
         let left   = num - 1
         let right  = num + 1
         let top    = num - matrix
         let bottom = num + matrix
-        if (left + 1)%matrix > 0, !self.blackList.contains(left), !self.routeList.contains(left) {
+        if (left + 1)%matrix > 0 {
             neighbours.append(left)
         }
-        if right%matrix > 0, !self.blackList.contains(right), !self.routeList.contains(right) {
+        if right%matrix > 0 {
             neighbours.append(right)
         }
-        if top >= 0, !self.blackList.contains(top), !self.routeList.contains(top) {
+        if top >= 0 {
             neighbours.append(top)
         }
-        if bottom < matrix * matrix, !self.blackList.contains(bottom), !self.routeList.contains(bottom) {
+        if bottom < matrix * matrix {
             neighbours.append(bottom)
         }
-        print("坐标\(num), 四周可用坐标有\(neighbours)")
         return neighbours
     }
     
-    //print(nextStepList(0))
-    mutating func findRoute(start index: Int) {
-        print("我现在添加了坐标:\(index)")
-        self.routeList.append(index)
-        if routeList.count >= word.count {
-            print("找齐了")
-            return
-        }
-        let list = self.nextStepList(index)
-        for nextStep in list {
-            if routeList.count >= word.count {break}
-            if self.nextStepList(nextStep).count > 0 {
-                self.findRoute(start: nextStep)
-                self.blackList.removeAll()
-            } else {
-                print("找错了,删除:\(index)")
-                self.routeList.removeLast()
-                self.blackList.append(index)
-                guard let lastStep = self.routeList.last else {
-                    break
-                }
-                print("回退到上个数字:\(lastStep)")
-                self.findRoute(start: lastStep)
+    mutating func findRoute(_ index: Int) {
+        if !self.routeList.contains(index) {
+            print("我现在添加了坐标:\(index)")
+            self.routeList.append(index)
+            if self.routeList.count >= self.word.count {
+                return
             }
         }
+        // 获得有效数组
+        var list = self.nextStepList(index)
+        list = self.removeInvaildIndex(current: index, indexList: list)
+        print("坐标\(index), 四周可用坐标有\(list)")
+        if list.isEmpty {
+            // 1、先清空对应的黑名单列表
+            self.blackList2[index] = []
+            // 2、移除路径的最后一个数
+            let blackIndex = self.routeList.removeLast()
+            print("这条路死了,删除:\(blackIndex)")
+            guard let lastStep = self.routeList.last else {
+                return
+            }
+            // 3、将其添加到黑名单中
+            self.blackList2[lastStep].append(blackIndex)
+            print("回退到上个数字:\(lastStep)")
+            // 4、使用上一个有效数组查找
+            self.findRoute(lastStep)
+        } else {
+            for nextStep in list {
+                // 如果找齐了,则跳出循环
+                 if routeList.count >= word.count {
+                     print("找齐了!!!!!!!🎂")
+                     break
+                 }
+                self.findRoute(nextStep)
+            }
+        }
+        
     }
+    
+    /// 去除无效坐标
+    private func removeInvaildIndex(current index: Int, indexList: [Int]) -> [Int] {
+        let blackList = blackList2[index]
+        let validList = indexList.filter { (_index) -> Bool in
+            return !routeList.contains(_index) && !blackList.contains(_index) // 并且黑名单也不包含
+        }
+        return validList
+        
+    }
+
 }
